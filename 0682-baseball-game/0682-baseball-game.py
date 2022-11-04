a8150020687c17dd44b1fb0f2554d7cb7cc6d71e
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2022-11-03 18:02:34 -0700

    Time: 69 ms (72.36%), Space: 14.1 MB (74.00%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        scoreboard = []
+        for i, score in enumerate(operations):
+            if score == "C":
+                scoreboard.pop()
+            elif score == "D":
+                scoreboard.append(int(scoreboard[len(scoreboard)-1]) * 2)
+            elif score == "+":
+                scoreboard.append(int(scoreboard[len(scoreboard)-1]) + int(scoreboard[len(scoreboard)-2]))
+            else:
+                scoreboard.append(int(score))
+        
+        total_score = 0
+        for score in scoreboard:
+            total_score += score
+            
+        return total_score
