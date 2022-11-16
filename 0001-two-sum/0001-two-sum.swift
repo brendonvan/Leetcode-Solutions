06cccdcc5b58a1718e318f893e62d68b4767fae5
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2022-11-16 10:21:42 -0700

    Time: 103 ms (58.24%), Space: 14.6 MB (31.41%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+
+        for (i, value) in nums.enumerated() {
+            if let index = dict[target - value] {
+                return [index, i]
+            } else {
+                dict[value] = i
+            }
+        }
+    
+    return []
+    }
+}
