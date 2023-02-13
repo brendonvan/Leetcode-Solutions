db071921db4b17b740f888093f35d732e974c0c1
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-02-13 12:25:13 -0700

    Time: 139 ms (95.15%), Space: 14.3 MB (22.57%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,8 +5,12 @@ class Solution:
             return s
         start, max_len = 0, 1
         for i in range(n):
+            print(i)
+            
             odd = s[i - max_len - 1: i + 1]
             even = s[i - max_len: i + 1]
+            print(odd)
+            print(even)
             if i - max_len - 1 >= 0 and odd == odd[::-1]:
                 start = i - max_len - 1
                 max_len += 2
