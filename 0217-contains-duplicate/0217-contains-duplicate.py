2982051c450a596292bacc521f44fd64642c3cd8
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-02-17 13:08:56 -0700

    Time: 589 ms (33.72%), Space: 31.5 MB (8.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,30 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
-        # I like to start with the brute force solution first and then optimizing it later
-        
-        # Go through each number in array. 
-        # Each time you check a number, check if the original array contains a duplicate element.
-        # for idx, num in enumerate(nums): 
-        #     numsCopy = nums.copy()
-        #     numsCopy.pop(idx)
-        #     if num in numsCopy:
-        #         return True
-        # return False
-
-        # To optimize it I would:
-        # use a hashset to store a number if it doesn't exist, if it does return True
-
-        hashset = set()
-
-        for n in nums:
-            if n in hashset:
+        hash_table = {}
+        for p in range(len(nums)):
+            if hash_table.get(nums[p]):
                 return True
             else:
-                hashset.add(n)
+                hash_table[nums[p]] = True
         return False
-
-
-        # I know a quick one-liner for this question it looks like this:
-
-        # return len(nums) != len(set(nums))
