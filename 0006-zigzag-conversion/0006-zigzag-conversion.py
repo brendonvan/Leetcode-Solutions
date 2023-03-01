bf7553ca3e8f7733276e159c7d0f3f9cf93c4559
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-02-28 18:55:42 -0700

    Time: 52 ms (84.99%), Space: 14.2 MB (27.22%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        rr = range(numRows)
+        pattern = list(rr) + list(range(numRows-2, 0, -1))
+        times = math.ceil(len(s)/len(pattern))
+        patterns = pattern*times
+        
+        arr = ["" for _ in rr] 
+
+        for a,b in zip(patterns, s):
+            arr[a] += b
+        
+        return "".join(arr)
