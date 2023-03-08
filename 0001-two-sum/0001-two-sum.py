780dd875bf62cbffe799523d6623e8db0aa0c6d6
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-03-08 11:05:45 -0700

    Time: 75 ms (47.98%), Space: 15.4 MB (6.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
         
-        for pointer_one in range(len(nums)):
-            for pointer_two in range(pointer_one):
-
-                if (nums[pointer_one] + nums[pointer_two]) == target:
-                    return [pointer_one, pointer_two]
+        for i, n in enumerate(nums):
+            # check index and number
+            print(i, n)
+            
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
         
+            
+            
