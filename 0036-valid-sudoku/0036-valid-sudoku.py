124dab4460e20da67b4ace43a3a1fdcbe23162e5
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-03-10 10:54:24 -0700

    Time: 100 ms (53.95%), Space: 14 MB (24.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,6 +6,7 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
+                print(board[r][c])
                 if board[r][c] == ".":
                     continue
                 if (
