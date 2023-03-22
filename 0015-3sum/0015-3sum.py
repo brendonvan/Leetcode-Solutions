ea8c91604d1f55cf9966490bfdccd205be6cc35d
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-03-22 12:14:30 -0700

    Time: 1078 ms (90.12%), Space: 18.4 MB (39.41%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,76 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        solution = []
+        
+        
+        # sort list to allow for use of two pointers with sum
         nums.sort()
-
-        for i, a in enumerate(nums):
-            # Skip positive integers
-            if a > 0:
+        print(nums)
+        
+        for i, n in enumerate(nums):
+            # start on first index
+            # left pointer set to next number
+            # right pointer set to last number
+            
+            if n > 0:
                 break
 
-            if i > 0 and a == nums[i - 1]:
+            if i > 0 and n == nums[i - 1]:
                 continue
-
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
+            
+            l = i + 1
+            r = len(nums) - 1
+            
+            while(l < r):
+                
+                # if sum of left and right is greater than 0
+                sum = nums[i] + nums[l] + nums[r]
+                
+                if sum < 0:
                     l += 1
+                elif sum > 0:
+                    r -= 1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    solution.append([nums[i], nums[l], nums[r]])
+                    
+                    # check others in this starting index
                     l += 1
                     r -= 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        return res
-        
-        #         solution = []
-        
-        
-#         # sort list to allow for use of two pointers with sum
-#         nums.sort()
-#         print(nums)
-        
-#         for i, n in enumerate(nums):
-        
-#             # print(i,n)
-#             # start on first index
-#             # left pointer set to next number
-#             # right pointer set to last number
-#             if n > 0:
-#                 break
-
-#             if i > 0 and n == nums[i - 1]:
-#                 continue
             
-#             l = i + 1
-#             r = len(nums) - 1
-#             print(l, r)
+        return solution
             
-#             while(l < r):
-                
-#                 # if sum of left and right is greater than 0
-#                 sum = nums[i] + nums[l] + nums[r]
-                
-#                 print(nums[i], nums[l], nums[r], sum)
-                
-#                 if sum < 0:
-#                     l += 1
-#                 elif sum > 0:
-#                     r -= 1
-#                 else:
-#                     solution.append([nums[i], nums[l], nums[r]])
-#                     l += 1
-#                     r -= 1
-#                     while nums[l] == nums[l - 1] and l < r:
-#                         l += 1
-            
-#         print(solution)
-#         return solution
-            
-           
-            
-            # if first, left, right pointer sum to 0 append to solution list
+          
             
