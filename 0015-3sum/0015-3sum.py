7bce71349d6bed9203205aca609ac483d535860e
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-03-22 12:17:01 -0700

    Time: 1088 ms (89.19%), Space: 18.4 MB (39.41%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,22 +2,22 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         solution = []
         
-        
         # sort list to allow for use of two pointers with sum
         nums.sort()
         print(nums)
         
         for i, n in enumerate(nums):
             # start on first index
-            # left pointer set to next number
-            # right pointer set to last number
             
+            # if
             if n > 0:
                 break
 
             if i > 0 and n == nums[i - 1]:
                 continue
             
+            # left pointer set to next number
+            # right pointer set to last number
             l = i + 1
             r = len(nums) - 1
             
@@ -25,6 +25,8 @@ class Solution:
                 
                 # if sum of left and right is greater than 0
                 sum = nums[i] + nums[l] + nums[r]
+                
+                # print(nums[i], nums[l], nums[r], sum)
                 
                 if sum < 0:
                     l += 1
@@ -39,6 +41,7 @@ class Solution:
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
             
+        # print(solution)
         return solution
             
           
