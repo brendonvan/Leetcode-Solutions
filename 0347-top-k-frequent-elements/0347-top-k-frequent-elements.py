8475f525bbd561fef43e1fd21e026ff0b695f6c6
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-03-24 10:59:50 -0700

    Time: 104 ms (73.67%), Space: 18.8 MB (43.11%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,22 @@
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # key: number    value: frequency
-        hashMap = {}
         
-        for num in nums:
-            hashMap[num] = hashMap.get(num, 0) + 1
         
-        return list(dict(sorted(hashMap.items(), key=lambda x:x[1])))[-k:]
-
-        # O(n)
+        # create hashmap and store key as number, and value as frequency
+        # turn hashmap into tuples array
+        # sort the tuples by values using regex
+        # turn into array of only keys in the same order of the sorted array
+        # return k amount of keys of the most freq
+        
+        hashmap = {}
+        
+        for n in nums:
+            hashmap[n] = hashmap.get(n, 0) + 1
+            
+        freq_list = list(hashmap.items())
+        
+        freq_list.sort(reverse = False, key = lambda x: x[1])
+        
+        solution = [i[0] for i in freq_list][-k:]
+        return solution
