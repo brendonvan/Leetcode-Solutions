a68b35b189ddb443f8c66b410b0ce37e08c7497e
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-04-12 10:47:29 -0700

    Time: 33 ms (97.26%), Space: 14.2 MB (50.92%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -6,11 +6,12 @@ class Solution:
         while start < end:
             # find middle
             mid = (start + end) // 2
+            # grab current min
             curr_min = min(curr_min, nums[mid])
             
+            # if mid is greater than end that means min number is on the right side of mid
             if nums[mid] > nums[end]:
                 start = mid + 1
-            
             else:
                 end = mid - 1
         return min(curr_min, nums[start])
