81e57785fdb1308fad9b06e159455965d101b91b
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-04-17 11:04:17 -0700

    Time: 44 ms (21.87%), Space: 14.3 MB (20.64%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,19 +4,49 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        prehead = ListNode(-1)
-
-        prev = prehead
-        while list1 and list2:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:      
+        # check each current head
+        # if head is less than or equal to other
+        # add to LinkedList and on that list assign .next to current node
+        
+        dummy = ListNode()
+        res = dummy
+        
+        while (list1 and list2):
+            
             if list1.val <= list2.val:
-                prev.next = list1
+                res.next = list1
+                print(res.val)
                 list1 = list1.next
             else:
-                prev.next = list2
-                list2 = list2.next            
-            prev = prev.next
+                res.next = list2
+                print(res.val)
+                list2 = list2.next 
+                
+            res = res.next
+                
+        if list1:
+            res.next = list1
+        elif list2:
+            res.next = list2
+            
+        return dummy.next
+                
+#         dummy = ListNode()
+#         tail = dummy
 
-        prev.next = list1 if list1 is not None else list2
+#         while list1 and list2:
+#             if list1.val < list2.val:
+#                 tail.next = list1
+#                 list1 = list1.next
+#             else:
+#                 tail.next = list2
+#                 list2 = list2.next
+#             tail = tail.next
 
-        return prehead.next
+#         if list1:
+#             tail.next = list1
+#         elif list2:
+#             tail.next = list2
+
+#         return dummy.next
