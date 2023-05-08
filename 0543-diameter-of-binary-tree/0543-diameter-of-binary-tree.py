f3b96f7d1c0474ea565af8c0438ed537bf598262
Brendon Van <108159910+brendonvan@users.noreply.github.com>
2023-05-08 11:21:49 -0700

    Time: 64 ms (13.98%), Space: 18.7 MB (7.48%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -18,6 +18,26 @@ class Solution:
             
             return 1 + max(left, right)
         
+#             left = dfs(2) returns 1
+#             right = dfs(3) returns 0
+#             res[0] = max(res[0], 2 + 1 + 0)
+            
+#             return 1 + max(left, right)
+
+#             left = dfs(4) returns 0
+#             right = dfs(5) returns 0
+#             res[0] = max(res[0], 2 + left + right)
+            
+#             return 1 + max(left, right)
+        
+        
+        #.         1 root
+        #.        /.\
+        #        2.  3  
+        #.      / \.  
+        #.     4.  5
+        
+        
         dfs(root)
         return res[0]
         
